eb191b2f143187221ac345a597dc7674f74b3353
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-06-08 11:56:03 -0700

    Time: 55 ms (78.89%), Space: 42 MB (38.57%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -3,12 +3,20 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-    var x = 0;
-    while (x*x <= num) {
-        if (x*x === num) {
+    if (num <= 1) {
+        return true;
+    }
+    var start = 0;
+    var end = num / 2;
+    while (start <= end) {
+        var mid = start + Math.floor((end - start) / 2);
+        if (mid * mid === num) {
             return true;
+        } else if (mid * mid < num) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
         }
-        x++;
     }
     return false;
 };
