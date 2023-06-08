dbacf6ac91e3da66881b117a8f0537ab851fb471
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-06-07 23:06:53 -0700

    Time: 71 ms (5.93%), Space: 44.2 MB (5.17%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    // minimum is the point where nums[i+1] < nums[i]
+    var start = 0;
+    var end = nums.length - 1;
+    if (end < 2) {
+        return Math.min(nums[start], nums[end]);
+    }
+    if (nums[start] < nums[end]) {
+        return nums[start];
+    }
+    while (start < end) {
+        var mid = Math.floor(start + (end - start) / 2);
+        console.log(start, mid, end);
+        if (mid + 1 <= nums.length && nums[mid] > nums[mid + 1]) {
+            return nums[mid + 1];
+        } else if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) {
+            return nums[mid];
+        } else if (nums[start] < nums[end]) {
+            return nums[start];
+        } else if (nums[start] < nums[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return nums[start];
+};
