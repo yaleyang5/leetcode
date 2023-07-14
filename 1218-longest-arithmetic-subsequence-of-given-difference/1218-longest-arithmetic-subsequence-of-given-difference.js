8457814f1a5398504d6f09dd7cd5b4cee9970251
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-07-14 12:29:53 -0700

    Time: 130 ms (54.69%), Space: 52.7 MB (60.94%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.js
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.js
@@ -4,19 +4,36 @@
  * @return {number}
  */
 var longestSubsequence = function(arr, difference) {
-    // iterate just once (probably still too slow)
+    // iterative method - similar to twoSum
+    // create object keeping track of lengths of subsequences starting at a certain element (strategy below)
     var lens = {};
+    // current max
     var max = 1;
+    // first element's longest subsequence is 1
     lens[arr[0]] = 1;
     for (var i = 1; i < arr.length; i++) {
+        // checking whether (current element - difference) is defined or not
         var curr = lens[arr[i] - difference]
         if (curr !== undefined) {
+            // if defined, increase the length of the subsequence starting at that element
             lens[arr[i]] = curr + 1;
+            // update max
             max = Math.max(max, curr + 1);
         } else {
+            // if not defined, longest arithmetic subsequence length of any single element is 1
             lens[arr[i]] = 1;
         }
-        // console.log(max, i, lens)
     }
     return max;
 };
+
+// strategy:
+// object: keys = what we are looking for; "next" element that will continue the subsequence
+// values = current length of that subsequence
+// example: [1, 3, 8, 5], diff = 2
+// {1: 1}, max = 1 <- [1] (first element we do not subtract difference from)
+// 3 - diff = 1: found!
+// {1:1, 3:2}, max = 2 <- [1, 3]
+// {1:1, 3:2, 8:1}, max = 2 <- [1, 3], [8]
+// {1:1, 3:2, 8:1, 5:3}, max = 3 <- [1, 3, 5], [8]
+// return max
