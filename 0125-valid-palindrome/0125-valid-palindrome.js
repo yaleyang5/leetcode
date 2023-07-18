87ca3e9c14a40aa6ce0c941ec2114b0794fff96e
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-07-18 12:18:33 -0700

    Time: 96 ms (10.47%), Space: 50 MB (15.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,15 +3,25 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    var forward = '';
-    var backward = '';
-    for (var i = 0; i < s.length; i++) {
-        var char = s[i];
-        if (char.toLowerCase() !== char.toUpperCase() || (char >= '0' && char <= '9')) {
-            forward += char.toLowerCase();
-            backward = char.toLowerCase() + backward;
-        }
+    if (s.length === 1 || s.length === 0) {
+        return true;
     }
-    // console.log(forward, backward);
-    return forward === backward;
+    var alphas = 0;
+    const alphanumeric = /^[\p{L}\p{N}]*$/u;
+    var j = s.length - 1;
+    for (var i = 0; i < s.length && j >= 0; i++) {
+        while (i + 1 < s.length && !s[i].toLowerCase().match(alphanumeric)) {
+            i++;
+        } 
+        while (j - 1 >= 0 && !s[j].toLowerCase().match(alphanumeric)) {
+            j--;
+        }
+        // console.log(s[i],s[j]);
+        if (s[i].match(alphanumeric) && s[j].match(alphanumeric) && s[i].toLowerCase() !== s[j].toLowerCase()) {
+            return false;
+        }
+
+        j--;
+    }
+    return true;
 };
