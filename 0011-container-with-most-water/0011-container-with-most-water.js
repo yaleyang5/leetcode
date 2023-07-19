8ea9fac904225275b475f972784a5e0314176707
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-07-18 19:47:33 -0700

    Time: 77 ms (61.18%), Space: 49.9 MB (43.45%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    // area = (height[j] - height[i]) * (Math.min(height[j], height[i]));
+    var start = 0;
+    var end = height.length - 1;
+    var max = (end - start) * (Math.min(height[start], height[end]));
+    while (start <= end) {
+        var area = (end - start) * (Math.min(height[start], height[end]));
+        max = Math.max(max, area);
+        if (height[start] < height[end]) {
+            start++;
+        } else if (height[start] > height[end]) {
+            end--;
+        } else {
+            if (height[start + 1] <= height[end - 1]) {
+                start++
+            } else {
+                end--;
+            }
+        }
+    }
+    return max;
+};
