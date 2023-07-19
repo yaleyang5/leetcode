4e933ce19e98121aeaba4a645e8842aa6a7e924f
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-07-18 23:34:46 -0700

    Time: 71 ms (96.11%), Space: 51.9 MB (25.73%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,24 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    // walk through once:
-    // create min, max, and diff variables
-    // min starts at prices[0], max starts at 0
-    var min = prices[0];
-    var max = 0;
     var diff = 0;
-    // keep reducing min until larger num found - if so, set max and diff
-    // look in this order: 
-        // min? update min, clear max.
-        // max? update diff
+    var min = 2147483647;
+    var max= -2147483648;
     for (var i = 0; i < prices.length; i++) {
-        var price = prices[i];
-        if (price < min) {
-            min = price;
-        } else {
-            max = price;
-            diff = Math.max(max - min, diff);
+        var curr = prices[i];
+        if (min !== 2147483647) {
+            diff = Math.max(diff, curr - min);
         }
+        min = Math.min(min, curr);
+        max = Math.max(max, curr);
     }
     return diff;
 };
