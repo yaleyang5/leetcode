42aa275111f4cb532a46257b31181cecdc0fdee6
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-07-20 19:57:46 -0700

    Time: 78 ms (85.93%), Space: 46.6 MB (74.60%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,34 +3,17 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    if (s === undefined) {
-        return 0;
-    }
-    var len = s.length;
-    if (len <= 1) {
-        return len;
-    }
-    if (len === 2) {
-        return s[0] !== s[1] ? 2 : 1;
-    }
-    var first = 0;
-    var last = 1;
-    var max = 1;
-    var letters = {};
-    letters[s[0]] = 1;
-    while (first <= last && last < s.length) {
-        // console.log(first, last, letters, max);
-        if (letters[s[last]] > 0) {
-            letters[s[last]]++;
-            while(letters[s[last]] !== 1) {
-                letters[s[first]] -= 1;
-                first++;
-            }
-        } else {
-            letters[s[last]] = 1;
-            max = Math.max(max, last - first + 1);
+    var set = new Set();
+    var start = 0;
+    var result = 0;
+    
+    for (var i = 0; i < s.length; i++) {
+        while (set.has(s[i])) {
+            set.delete(s[start]);
+            start++;
         }
-        last++;
+        set.add(s[i]);
+        result = Math.max(result, i - start + 1);
     }
-    return max;
+    return result;
 };
