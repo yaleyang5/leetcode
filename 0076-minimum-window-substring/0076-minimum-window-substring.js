db0a171fe4f725555eabdb02942af9cb0515db33
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-07-22 19:21:47 -0700

    Time: 94 ms (76.36%), Space: 45.6 MB (73.72%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -7,50 +7,47 @@ var minWindow = function(s, t) {
     if (t.length > s.length) {
         return "";
     }
-    
     var l = 0;
+    var have = 0;
+    var need = 0;
+    var sSet = {};
     var tSet = {};
+    var result = [0, s.length];
+    var len = s.length + 1;
     for (var i = 0; i < t.length; i++) {
         var curr = t[i];
         if (tSet[curr] === undefined) {
             tSet[curr] = 0;
+            need++;
         }
-        tSet[curr] += 1;
+        tSet[curr]++;
     }
-    // console.log(tSet);
-    var min = [0, s.length + 1];
-    var sSet = {};
-    for (var i = 0; i < s.length; i++) {
-        // console.log(s.substring(l, i + 1));
-        var end = s[i];
-        if (sSet[end] === undefined) {
-            sSet[end] = 0;
+    // console.log(tSet, need);
+    for (var r = 0; r < s.length; r++) {
+        var temp = s[r];
+        if (sSet[temp] === undefined) {
+            sSet[temp] = 0;
         }
-        sSet[end] += 1;
-        // console.log(sSet);
-        var found = true;
-        for (var key in tSet) {
-            if (sSet[key] === undefined || sSet[key] < tSet[key]) {
-                found = false;
-            }
+        sSet[temp]++;
+        if (sSet[temp] === tSet[temp]) {
+            have++;
         }
-        // iterate left until we find a character that is in tSet. record minimum, continue right pointer
-        while (found && l <= i) {
-            var start = s[l];
-            // console.log("found!", l, i);
-            if (tSet[start] !== undefined && sSet[start] !== undefined && tSet[start] <= sSet[start]) {
-                if (min[1] - min[0] + 1 > i - l + 1) {
-                    min[0] = l;
-                    min[1] = i;
-                }
+        
+        while (have === need) {
+            var firstChar = s[l]
+            // record length
+            if (len > r - l + 1) {
+                len = r - l + 1;
+                result[0] = l;
+                result[1] = r;
             }
-            sSet[start] -= 1;
+            // check and remove first char
+            if (sSet[firstChar] === tSet[firstChar]) {
+                have--;
+            }
+            sSet[firstChar] -= 1;
             l++;
-            if (l <= i && sSet[s[l - 1]] < tSet[s[l - 1]]) {
-                found = false;
-            }
         }
     }
-    // console.log(min[0], min[1]);
-    return min[1] === s.length + 1 ? "" : s.substring(min[0], min[1] + 1);
+    return len === s.length + 1 ? "" : s.substring(result[0], result[1] + 1);
 };
