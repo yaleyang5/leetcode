36f3fac3dac20b8a0986c925c16b66592448b6a3
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-07-23 18:37:15 -0700

    Time: 62 ms (55.56%), Space: 41.8 MB (69.16%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -10,12 +10,18 @@ var myPow = function(x, n) {
     if (n === 0 || x === 1) {
         return 1;
     }
+    if (x === 1) {
+        return 1;
+    }
     if (x === -1) {
         return n % 2 === 0 ? 1 : -1;
     }
-    var pow = 1;
-    for (var i = 0; i < Math.abs(n); i++) {
-        pow *= x;
+    if (n < 0) {
+        return 1 / myPow(x, -n);
     }
-    return n < 0 ? 1 / pow : pow;
+    if (n % 2 == 0) {
+        return myPow(x * x, n / 2);
+    } else {
+        return x * myPow(x * x, (n - 1) / 2);
+    }
 };
