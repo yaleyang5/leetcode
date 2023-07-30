c2db352212c72c8c4368fc423ea65be1d8f43ab7
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-07-30 16:40:15 -0700

    Time: 48 ms (94.67%), Space: 42.1 MB (48.21%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -3,29 +3,23 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    // minimum is the point where nums[i+1] < nums[i]
+    // use binary search to find element where previous > curr
+    if (nums[0] <= nums[nums.length - 1]) {
+        return nums[0];
+    }
     var start = 0;
     var end = nums.length - 1;
-    if (end < 2) {
-        return Math.min(nums[start], nums[end]);
-    }
-    if (nums[start] < nums[end]) {
-        return nums[start];
-    }
-    while (start < end) {
-        var mid = Math.floor(start + (end - start) / 2);
-        // console.log(start, mid, end);
-        if (mid + 1 <= nums.length && nums[mid] > nums[mid + 1]) {
-            return nums[mid + 1];
-        } else if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) {
+    while (start <= end) {
+        var mid = start + Math.floor((end - start) / 2);
+        if (mid - 1 >= 0 && nums[mid - 1] > nums[mid]) {
             return nums[mid];
-        } else if (nums[start] < nums[end]) {
-            return nums[start];
-        } else if (nums[start] < nums[mid]) {
-            start = mid + 1;
-        } else {
+        } else if (mid + 1 < nums.length && nums[mid] > nums[mid + 1]) {
+            return nums[mid + 1];
+        } else if (nums[start] > nums[end] && nums[start] > nums[mid]) {
             end = mid - 1;
+        } else {
+            start = mid + 1;
         }
     }
-    return nums[start];
+    return -1;
 };
