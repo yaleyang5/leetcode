49376d37e367c1b68d5c0c68f5f97d59cd599b36
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-08-03 18:31:15 -0700

    Time: 71 ms (64.36%), Space: 44.9 MB (64.21%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -16,7 +16,7 @@ var hasCycle = function(head) {
     while (fast !== null && fast.next !== null && fast.next.next !== null) {
         slow = slow.next;
         fast = fast.next.next;
-        if (slow == fast) {
+        if (slow === fast) {
             return true;
         }
     }
