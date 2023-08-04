7d7e4c1bb2f178f0324de53e4782dac4c0bbc5ce
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-08-04 03:33:24 -0700

    Time: 126 ms (5.19%), Space: 61.2 MB (5.76%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    console.log(root)
+    if (root === null) {
+        return root;
+    }
+    if (p.val >= root.val && q.val <= root.val || p.val <= root.val && q.val >= root.val) {
+        return root;
+    } else if (p.val > root.val) {
+        return lowestCommonAncestor(root.right, p, q);
+    } else {
+        return lowestCommonAncestor(root.left, p, q);
+    }
+};
