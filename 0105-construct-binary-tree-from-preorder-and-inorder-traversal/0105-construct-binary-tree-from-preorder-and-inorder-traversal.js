006754070e2ffa8171b5bbb7fa0b295b0523f9d4
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-08-05 20:17:26 -0700

    Time: 86 ms (70.74%), Space: 136.5 MB (20.83%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -15,11 +15,9 @@ var buildTree = function(preorder, inorder) {
     if (inorder.length === 0 || preorder.length === 0) {
         return null;
     }
-    var root = preorder.shift();
-    var index = inorder.indexOf(root);
-    
-    var node = new TreeNode(root);
-    node.left = buildTree(preorder.slice(0, index), inorder.slice(0, index));
-    node.right = buildTree(preorder.slice(index, preorder.length), inorder.slice(index + 1, inorder.length));
-    return node;
+    var index = inorder.indexOf(preorder[0]);
+    var root = new TreeNode(preorder[0]);
+    root.left = buildTree(preorder.slice(1, index + 1), inorder.slice(0, index));
+    root.right = buildTree(preorder.slice(index + 1, preorder.length), inorder.slice(index + 1, inorder.length));
+    return root;
 };
