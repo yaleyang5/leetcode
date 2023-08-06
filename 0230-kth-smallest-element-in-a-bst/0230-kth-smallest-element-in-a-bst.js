b17d4ef9e61eb98b40a73fe751e067f02c1f74d3
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-08-05 19:49:53 -0700

    Time: 71 ms (69.83%), Space: 50.5 MB (5.95%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.js
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.js
@@ -12,15 +12,16 @@
  * @return {number}
  */
 var kthSmallest = function(root, k) {
-    // search inorder, return(k - 1)th index
+    // place in order, return(k - 1)th index
     return inOrder(root)[k - 1];
 };
+
 var inOrder = (root) => {
-        if (root === null) {
-            return [];
-        }
-        if (root.left === null && root.right === null) {
-            return [root.val];
-        }
-        return inOrder(root.left).concat(root.val).concat(inOrder(root.right));
+    if (root === null) {
+        return [];
     }
+    if (root.left === null && root.right === null) {
+        return [root.val];
+    }
+    return inOrder(root.left).concat(root.val).concat(inOrder(root.right));
+};
