173f71dc6cd25c8239e274d782e281b51721a876
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-08-05 19:25:05 -0700

    Time: 57 ms (94.69%), Space: 46.5 MB (58.53%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -11,28 +11,15 @@
  * @return {boolean}
  */
 var isValidBST = function(root) {
-    if (root === null || (root.left === null && root.right === null)) {
-        return true;
+    isValid = (node, left, right) => {
+        if (node === null) {
+            return true;
+        }
+        if (node.val <= left || node.val >= right) {
+            return false;
+        }
+        return isValid(node.left, left, node.val) && isValid(node.right, node.val, right);
     }
-    if (root.left === null) {
-        return isGreater(root.right, root.val) && isValidBST(root.right);
-    }
-    if (root.right === null) {
-        return isLess(root.left, root.val) && isValidBST(root.left);
-    }
-    return isValidBST(root.left) && isValidBST(root.right) && isLess(root.left, root.val) && isGreater(root.right, root.val);
-};
-
-var isLess = (root, val) => {
-    if (root === null) {
-        return true;
-    }
-    return root.val < val && isLess(root.left, val) && isLess(root.right, val);
-};
-
-var isGreater = (root, val) => {
-    if (root === null) {
-        return true;
-    }
-    return root.val > val && isGreater(root.left, val) && isGreater(root.right, val);
+    
+    return isValid(root, -Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
 };
