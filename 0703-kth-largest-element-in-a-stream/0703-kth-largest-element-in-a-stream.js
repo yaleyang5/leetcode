370950f8cb2c6dd3332b2ff585cd7034171c7279
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-08-31 18:26:34 -0700

    Time: 133 ms (64.89%), Space: 52.4 MB (40.93%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
@@ -2,11 +2,11 @@
  * @param {number} k
  * @param {number[]} nums
  */
-
 var KthLargest = function(k, nums) {
-    this.nums = nums;
-    this.nums.sort((a,b) => (a - b));
+    this.minHeap = new MinPriorityQueue();
     this.k = k;
+    
+    nums.forEach((num) => this.add(num));
     return this;
 };
 
@@ -15,17 +15,23 @@ var KthLargest = function(k, nums) {
  * @return {number}
  */
 KthLargest.prototype.add = function(val) {
-    this.nums.push(val);
-    for (var i = this.nums.length - 2; i > -1; i--) {
-        if (this.nums[i + 1] < this.nums[i]) {
-            var temp = this.nums[i];
-            this.nums[i] = this.nums[i + 1];
-            this.nums[i + 1] = temp;
-        } else {
-            break;
-        }
+    if (this.minHeap.size() < this.k) {
+        this.minHeap.enqueue(val);
+        
+        return this.top();
     }
-    return this.nums[this.nums.length - this.k];
+    
+    if (this.top() < val) {
+        this.minHeap.dequeue();
+        this.minHeap.enqueue(val);
+    }
+    
+    return this.top();
+};
+
+
+KthLargest.prototype.top = function() {
+    return this.minHeap.front()?.element || 0;
 };
 
 /** 
