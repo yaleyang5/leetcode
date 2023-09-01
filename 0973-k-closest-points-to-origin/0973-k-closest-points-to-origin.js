0e188237900139cf9dc384ebb4049e24aa5608ac
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-08-31 19:17:28 -0700

    Time: 179 ms (71.03%), Space: 59.2 MB (95.06%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
@@ -4,7 +4,6 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-    var res = [];
     points.sort((a, b) => (Math.pow(a[0], 2) + Math.pow(a[1], 2)) - (Math.pow(b[0], 2) + Math.pow(b[1], 2)) );
     
     return points.slice(0, k);
