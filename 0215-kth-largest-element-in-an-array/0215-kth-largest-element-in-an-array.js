4d000c3f903daf329e0eebcd50062603430e2996
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-08-31 19:32:59 -0700

    Time: 167 ms (31.04%), Space: 70.8 MB (5.04%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -6,9 +6,8 @@
 var findKthLargest = function(nums, k) {
     var minPQ = new MinPriorityQueue();
     for (var i = 0; i < nums.length; i++) {
-        var num = nums[i];
-        if (minPQ.size() < k || num > minPQ.front().element) {
-            minPQ.enqueue(num);
+        if (minPQ.size() < k || nums[i] > minPQ.front().element) {
+            minPQ.enqueue(nums[i]);
         }
         if (minPQ.size() > k) {
             minPQ.dequeue();
