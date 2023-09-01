9b826f4359845ce778a9779253a675bda6f948c2
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-08-31 22:22:52 -0700

    Time: 52 ms (65.59%), Space: 41.7 MB (77.96%) - LeetHub

---
+++ 0355-design-twitter/0355-design-twitter.js
@@ -0,0 +1,69 @@
+
+var Twitter = function() {
+    this.maxPQ = new MaxPriorityQueue({priority: (tweet) => tweet.time});
+    this.following = {};
+    this.time = 0;
+    return this;
+};
+
+/** 
+ * @param {number} userId 
+ * @param {number} tweetId
+ * @return {void}
+ */
+Twitter.prototype.postTweet = function(userId, tweetId) {
+    this.maxPQ.enqueue({userId: userId, tweetId: tweetId, time: this.time});
+    this.time++;
+};
+
+/** 
+ * @param {number} userId
+ * @return {number[]}
+ */
+Twitter.prototype.getNewsFeed = function(userId) {
+    if (this.following[userId] === undefined) {
+        this.following[userId] = {};
+    }
+    var res = [];
+    var count = 10;
+    this.maxPQ.toArray().forEach((tweet) => {
+        if ((userId === tweet.element.userId || this.following[userId][tweet.element.userId] === true) && count > 0) {
+            res.push(tweet.element.tweetId);
+            count--;
+        }
+    })
+    return res;
+};
+
+/** 
+ * @param {number} followerId 
+ * @param {number} followeeId
+ * @return {void}
+ */
+Twitter.prototype.follow = function(followerId, followeeId) {
+    if (this.following[followerId] === undefined) {
+        this.following[followerId] = {};
+    }
+    this.following[followerId][followeeId] = true;
+};
+
+/** 
+ * @param {number} followerId 
+ * @param {number} followeeId
+ * @return {void}
+ */
+Twitter.prototype.unfollow = function(followerId, followeeId) {
+    if (this.following[followerId] === undefined) {
+        this.following[followerId] = {};
+    }
+    this.following[followerId][followeeId] = false;
+};
+
+/** 
+ * Your Twitter object will be instantiated and called as such:
+ * var obj = new Twitter()
+ * obj.postTweet(userId,tweetId)
+ * var param_2 = obj.getNewsFeed(userId)
+ * obj.follow(followerId,followeeId)
+ * obj.unfollow(followerId,followeeId)
+ */
