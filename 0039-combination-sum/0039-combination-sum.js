c65e6c08d5a7a8ac16eb1d0b49dc71c65e48d55c
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-09-01 19:32:29 -0700

    Time: 62 ms (88.80%), Space: 45.5 MB (49.90%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    var res = [];
+    
+    var dfs = (i, curr, total) => {
+        if (total === target) {
+            res.push(curr.slice());
+            return;        
+        }
+        if (i >= candidates.length || total > target) {
+            return;
+        }
+        
+        curr.push(candidates[i]);
+        dfs(i, curr, total + candidates[i]);
+        curr.pop();
+        dfs(i + 1, curr, total);
+    }
+    
+    dfs(0, [], 0);
+    return res;
+};
