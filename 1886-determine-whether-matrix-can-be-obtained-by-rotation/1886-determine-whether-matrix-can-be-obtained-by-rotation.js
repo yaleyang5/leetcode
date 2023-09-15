dae1b99fc7e7a8001ea16850d2bfa72b28f7f5ab
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-09-15 16:07:05 -0700

    Time: 56 ms (56.92%), Space: 41.7 MB (96.92%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.js
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.js
@@ -4,32 +4,22 @@
  * @return {boolean}
  */
 var findRotation = function(mat, target) {
-    var rot0 = true;
-    var rot1 = true;
-    var rot2 = true;
-    var rot3 = true;
+    var rots = [true, true, true, true];
     var n = mat.length;
     for (var i = 0; i < n*n; i++) {
         var x = Math.floor(i / n);
         var y = i % n;
         var t = target[x][y]
-        rot0 = rot0 && mat[x][y] === t;
-        [x, y] = rotate(x, y, n);
-        rot1 = rot1 && mat[x][y] === t;
-        [x, y] = rotate(x, y, n);
-        rot2 = rot2 && mat[x][y] === t;
-        [x, y] = rotate(x, y, n);
-        rot3 = rot3 && mat[x][y] === t;
+        rots[0] = rots[0] && mat[x][y] === t;
+        for (var j = 1; j < 4; j++) {
+            // reflect over x axis
+            x = n - 1 - x;
+            // reflect over y = -x
+            var temp = y
+            y = x;
+            x = temp;
+            rots[j] = rots[j] && mat[x][y] === t;
+        }
     }
-    return rot0 || rot1 || rot2 || rot3;
+    return rots[0] || rots[1] || rots[2] || rots[3];
 };
-
-var rotate = (x, y, n) => {
-     // reflect over x axis
-    x = n - 1 - x;
-    // reflect over y = -x
-    var temp = y
-    y = x;
-    x = temp;
-    return [x, y];
-}
