778f2185fa46d4aa7c147e337fcf8fca52ef8790
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-09-18 17:20:30 -0700

    Time: 68 ms (30.31%), Space: 44.9 MB (44.84%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -3,18 +3,18 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    if (nums.length === 0) {
-        return [[]];
+    if (nums === undefined || nums.length === 0) {
+        return [];
     }
     if (nums.length === 1) {
         return [[nums[0]]];
     }
     var result = [];
     for (var i = 0; i < nums.length; i++) {
-        var temp = permute(nums.slice(0, i).concat(nums.slice(i + 1, nums.length)));
-        for (var j = 0; j < temp.length; j++) {
-            temp[j] = [nums[i]].concat(temp[j]);
-            result.push(temp[j].slice());
+        var perms = permute(nums.slice(0, i).concat(nums.slice(i + 1, nums.length)));
+        for (var j = 0; j < perms.length; j++) {
+            var arr = [nums[i]].concat(perms[j].slice());
+            result.push(arr);
         }
     }
     return result;
