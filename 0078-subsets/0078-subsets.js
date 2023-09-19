2a71b73381224ad714e07afed45cee18684bb26b
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-09-19 00:33:29 -0700

    Time: 50 ms (86.76%), Space: 44.4 MB (13.56%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -4,19 +4,16 @@
  */
 var subsets = function(nums) {
     var res = [];
-    
-    var dfs = (i, curr) => {
-        if (i >= nums.length) {
-            res.push(curr.slice());
+    var dfs = (i, subset) => {
+        if (i === nums.length) {
+            res.push(subset.slice());
             return;
         }
-        curr.push(nums[i]);
-        dfs(i + 1, curr);
-        
-        curr.pop();
-        dfs(i + 1, curr);
+        subset.push(nums[i]);
+        dfs(i + 1, subset);
+        subset.pop();
+        dfs(i + 1, subset);
     }
     dfs(0, []);
-    
     return res;
 };
