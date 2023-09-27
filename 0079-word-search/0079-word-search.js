973a475025ef8c3bf23d4a32347ba729f3661263
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-09-26 20:16:33 -0700

    Time: 1921 ms (18.55%), Space: 48.3 MB (31.93%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -4,30 +4,69 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    var rows = board.length;
-    var cols = board[0].length;
-    
-    var path = new Set();
-    
-    var dfs = (r, c, i) => {
-        if (i === word.length) {
-            return true;
-        }
-        if (r < 0 || c < 0 || r >= rows || c >= cols || word[i] != board[r][c] || path.has(String(r) + ',' + String(c))) {
+    var n = board[0].length;
+    var dfs = (path, i, wordIndex) => {
+        // console.log(path, i, wordIndex);
+        if (wordIndex >= word.length) {
             return false;
         }
-        path.add(String(r) + ',' + String(c));
-        var res = dfs(r + 1, c, i + 1) ||
-            dfs(r - 1, c, i + 1) ||
-            dfs(r, c + 1, i + 1) ||
-            dfs(r, c - 1, i + 1);
-        path.delete(String(r) + ',' + String(c))
-        return res;
+        var u, d, l, r;
+        var x = Math.floor(i /n);
+        var y = i % n;
+        // up: make sure it is in bounds, the next char, and not in path
+        if (x - 1 >= 0 && board[x - 1][y] === word[wordIndex] && path[String(x - 1) + ',' + String(y)] !== 1) {
+            if (wordIndex + 1 === word.length) {
+                // if we found the last char
+                return true;
+            } else {
+                // we need to continue
+                path[String(x - 1) + ',' + String(y)] = 1;
+                u = dfs(path, i - n, wordIndex + 1);
+                path[String(x - 1) + ',' + String(y)] = 0;
+            }
+        }
+        // down
+        if (x + 1 < board.length && board[x + 1][y] === word[wordIndex] && path[String(x + 1) + ',' + String(y)] !== 1) {
+            if (wordIndex + 1 === word.length) {
+                return true;
+            } else {
+                path[String(x + 1) + ',' + String(y)] = 1;
+                d = dfs(path, i + n, wordIndex + 1);
+                path[String(x + 1) + ',' + String(y)] = 0;
+            }
+        }
+        // left
+        if (y - 1 >= 0 && board[x][y - 1] === word[wordIndex] && path[String(x) + ',' + String(y - 1)] !== 1) {
+            if (wordIndex + 1 === word.length) {
+                return true;
+            } else {
+                path[String(x) + ',' + String(y - 1)] = 1;
+                l = dfs(path, i - 1, wordIndex + 1);
+                path[String(x) + ',' + String(y - 1)] = 0;
+            }
+        }
+        // right
+        if (y + 1 < n && board[x][y + 1] === word[wordIndex] && path[String(x) + ',' + String(y + 1)] !== 1) {
+            if (wordIndex + 1 === word.length) {
+                return true;
+            } else {
+                path[String(x) + ',' + String(y + 1)] = 1;
+                r = dfs(path, i + 1, wordIndex + 1);
+                path[String(x) + ',' + String(y + 1)] = 0;
+            }
+        }
+        return u || d || l || r;
     }
-    
-    for (var r = 0; r < rows; r++) {
-        for (var c = 0; c < cols; c++) {
-            if (dfs(r, c, 0)) {
+    for (var i = 0; i < board.length * n; i++) {
+        var x = Math.floor(i / n);
+        var y = i % n;
+        if (board[x][y] === word[0]) {
+            if (word.length === 1) {
+                return true;
+            }
+            var path = {};
+            path[String(x) + ',' + String(y)] = 1;
+            if (dfs(path, i, 1)) {
                 return true;
             }
         }
