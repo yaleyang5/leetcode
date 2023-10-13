5f9d3b3fe0bf36c96aec35d4b626b3ce84e301ee
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-10-12 19:05:17 -0700

    Time: 1728 ms (21.66%), Space: 48.9 MB (14.82%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -4,28 +4,35 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    var m = board.length
+    var m = board.length;
     var n = board[0].length;
-    var path = {};
     
-    var dfs = (r, c, i) => {
-        if (i === word.length) {
-            return true;
-        }
-        var currPair = String(r) + ',' + String(c);
-        if (r < 0 || c < 0 || r >= m || c >= n || board[r][c] !== word[i] || path[currPair] === 1) {
+    var set = {};
+    
+    var dfs = (x, y, wordIndex) => {
+        // console.log(x, y, word.substring(0, wordIndex), set)
+        var curr = String(x) + "," + String(y);
+        if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] !== word[wordIndex] || set[curr] === 1) {
+            
             return false;
         }
-        path[currPair] = 1;
-        var res = (dfs(r + 1, c, i + 1) ||
-                  dfs(r - 1, c, i + 1) ||
-                  dfs(r, c + 1, i + 1) ||
-                  dfs(r, c - 1, i + 1));
-        path[currPair] = 0;
-        return res;
+        if (wordIndex === word.length - 1 && board[x][y] === word[wordIndex]) {
+            return true;
+        }
+        set[curr] = 1;
+        if (dfs(x + 1, y, wordIndex + 1) ||
+            dfs(x - 1, y, wordIndex + 1) ||
+            dfs(x, y + 1, wordIndex + 1) ||
+            dfs(x, y - 1, wordIndex + 1)) {
+            return true;
+        } else {
+            set[curr] = 0;
+            return false;
+        }
     }
     
     for (var i = 0; i < m * n; i++) {
+        set = {};
         var x = Math.floor(i / n);
         var y = i % n;
         if (dfs(x, y, 0)) {
