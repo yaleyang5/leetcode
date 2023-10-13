4083b136694a0ea3d0a9dcd8a7c745de35ff6dda
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-10-12 19:56:26 -0700

    Time: 45 ms (85.31%), Space: 41.9 MB (48.70%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -3,29 +3,37 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    if (digits.length === 0) {
-        return [];
+    var map = {
+        2: ["a", "b", "c"],
+        3: ["d", "e", "f"],
+        4: ["g", "h", "i"],
+        5: ["j", "k", "l"],
+        6: ["m", "n", "o"],
+        7: ["p", "q", "r", "s"],
+        8: ["t", "u", "v"],
+        9: ["w", "x", "y", "z"]
     }
-    var combos = {
-        2: "abc",
-        3: "def",
-        4: "ghi",
-        5: "jkl",
-        6: "mno",
-        7: "pqrs",
-        8: "tuv",
-        9: "wxyz"
-    };
-    if (digits.length === 1) {
-        return combos[digits[0]].split('');
-    }
-    var result = [];
-    // digits[i] is a number [ "2" ]
-    for (var i = 0; i < combos[digits[0]].length; i++) {
-        var digitsSlice = letterCombinations(digits.slice(1));
-        for (var j = 0; j < digitsSlice.length; j++) {
-            result.push(combos[digits[0]][i] + digitsSlice[j]);
+    
+    var res = [];
+    
+    var combo = "";
+    
+    var dfs = (i) => {
+        if (i === digits.length) {
+            if (combo.length > 0) {
+                res.push(combo.slice());
+            }
+            return;
+        }
+        var arr = map[digits[i]];
+        for (var j = 0; j < arr.length; j++) {
+            var temp = combo;
+            combo += arr[j];
+            dfs(i + 1);
+            combo = temp;
         }
     }
-    return result;
+    dfs(0);
+    
+    return res;
 };
