23d0e7d6ad653520602eb87b13ec979bd88f5990
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-10-13 10:44:27 -0700

    Time: 42 ms (94.42%), Space: 41.8 MB (58.31%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -4,14 +4,14 @@
  */
 var letterCombinations = function(digits) {
     var map = {
-        2: ["a", "b", "c"],
-        3: ["d", "e", "f"],
-        4: ["g", "h", "i"],
-        5: ["j", "k", "l"],
-        6: ["m", "n", "o"],
-        7: ["p", "q", "r", "s"],
-        8: ["t", "u", "v"],
-        9: ["w", "x", "y", "z"]
+        2: "abc",
+        3: "def",
+        4: "ghi",
+        5: "jkl",
+        6: "mno",
+        7: "pqrs",
+        8: "tuv",
+        9: "wxyz"
     }
     
     var res = [];
@@ -25,10 +25,10 @@ var letterCombinations = function(digits) {
             }
             return;
         }
-        var arr = map[digits[i]];
-        for (var j = 0; j < arr.length; j++) {
+        var str = map[digits[i]];
+        for (var j = 0; j < str.length; j++) {
             var temp = combo;
-            combo += arr[j];
+            combo += str[j];
             dfs(i + 1);
             combo = temp;
         }
