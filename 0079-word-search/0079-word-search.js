ed960ee19dc0b03f0f19ba9ce6ba66c859a5f3e9
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-10-12 19:10:21 -0700

    Time: 1626 ms (23.13%), Space: 48.2 MB (34.28%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -6,37 +6,31 @@
 var exist = function(board, word) {
     var m = board.length;
     var n = board[0].length;
-    
     var set = {};
     
-    var dfs = (x, y, wordIndex) => {
-        // console.log(x, y, word.substring(0, wordIndex), set)
+    var dfs = (x, y, i) => {
+        if (i === word.length) {
+            return true;
+        }
         var curr = String(x) + "," + String(y);
-        if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] !== word[wordIndex] || set[curr] === 1) {
-            
+        if (x < 0 || y < 0 || x >= m || y >= n || word[i] !== board[x][y] || set[curr] === 1) {
             return false;
         }
-        if (wordIndex === word.length - 1 && board[x][y] === word[wordIndex]) {
-            return true;
-        }
+        
         set[curr] = 1;
-        if (dfs(x + 1, y, wordIndex + 1) ||
-            dfs(x - 1, y, wordIndex + 1) ||
-            dfs(x, y + 1, wordIndex + 1) ||
-            dfs(x, y - 1, wordIndex + 1)) {
-            return true;
-        } else {
-            set[curr] = 0;
-            return false;
-        }
+        var res = (dfs(x + 1, y, i + 1) ||
+                  dfs(x - 1, y, i + 1) ||
+                  dfs(x, y + 1, i + 1) ||
+                  dfs(x, y - 1, i + 1));
+        set[curr] = 0;
+        return res;
     }
     
-    for (var i = 0; i < m * n; i++) {
-        set = {};
-        var x = Math.floor(i / n);
-        var y = i % n;
-        if (dfs(x, y, 0)) {
-            return true;
+    for (var x = 0; x < m; x++) {
+        for (var y = 0; y < n; y++) {
+            if (dfs(x, y, 0)) {
+                return true;
+            }
         }
     }
     return false;
