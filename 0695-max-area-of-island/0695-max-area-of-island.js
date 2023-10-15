517da5760b4cc46e29c98de00a6b91b08305bc2d
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-10-14 17:16:45 -0700

    Time: 68 ms (67.69%), Space: 44.9 MB (79.81%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -7,23 +7,22 @@ var maxAreaOfIsland = function(grid) {
     var n = grid[0].length;
     
     var max = 0;
-    var visited = {};
     
     var dfs = (x, y) => {
-        if (x < 0 || y < 0 || x >= m || y >= n || grid[x][y] === 0 || visited[x * n + y] !== undefined) {
+        if (x < 0 || y < 0 || x >= m || y >= n || grid[x][y] === 0 || grid[x][y] === 5) {
             return 0;
         }
         // count the current tile as visited and continue checking
-        visited[x * n + y] = 1;
+        grid[x][y] = 5;
         return 1 + dfs(x + 1, y) + dfs(x - 1, y) + dfs(x, y + 1) + dfs(x, y - 1);
         
     }
     
-    for (var i = 0; i < m * n; i++) {
-        var x = Math.floor(i / n);
-        var y = i % n;
-        if (grid[x][y] === 1 && visited[i] === undefined) {
-            max = Math.max(dfs(x, y), max);
+    for (var x = 0; x < m; x++) {
+        for (var y = 0; y < n; y++) {
+            if (grid[x][y] === 1) {
+                max = Math.max(dfs(x, y), max);
+            }
         }
     }
     return max;
