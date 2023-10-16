ce8d99d155033b29dee7a48d3dbbf221cf1cc256
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-10-16 00:40:05 -0700

    Time: 55 ms (80.82%), Space: 43.2 MB (27.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,19 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // iterate through nums
-    // how to store pairs of sums? key: target - value, value: index
-        // we then check if value exists in set
-        // if so, return current index and value
-    var values = {};
+    var sums = {};
     for (var i = 0; i < nums.length; i++) {
-        var value = nums[i];
-        var vIndex = values[value];
-        if (vIndex === undefined) {
-            values[target - value] = i;
-        } else {
-            return [i, vIndex];
+        if (sums[nums[i]] !== undefined) {
+            return [i, sums[nums[i]]];
         }
+        sums[target - nums[i]] = i;
     }
     return [-1, -1];
 };
