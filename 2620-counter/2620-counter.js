b7549756f74fdcea7c3cd9856e635ab10e05bf41
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-11-01 16:24:22 -0700

    Time: 55 ms (23.80%), Space: 42.1 MB (30.74%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -2,17 +2,10 @@
  * @param {number} n
  * @return {Function} counter
  */
-
-var called = 0;
-var prevN = null;
-
 var createCounter = function(n) {
-    if (n !== prevN) {
-        prevN = n;
-        called = 0;
-    }
+    var count = n;
     return function() {
-        return n + called++;
+        return count++;
     };
 };
 
