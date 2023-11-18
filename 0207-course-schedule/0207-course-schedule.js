a18e02000754123ebe55800f9b9c26b33c93e05e
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2023-11-17 18:16:01 -0800

    Time: 73 ms (43.12%), Space: 47.8 MB (50.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.js
+++ 0207-course-schedule/0207-course-schedule.js
@@ -4,35 +4,37 @@
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
-    var preReqs = {};
-    for (var i = 0; i < prerequisites.length; i++) {
-        if (preReqs[prerequisites[i][0]] === undefined) {
-            preReqs[prerequisites[i][0]] = [];
-        }
-        preReqs[prerequisites[i][0]].push(prerequisites[i][1]);
-    }
-    var set = new Set();
+    var preMap = {};
     for (var i = 0; i < numCourses; i++) {
-        if (preReqs[i] === undefined) {
-            set.add(i);
+        preMap[i] = [];
+    }
+    for (var [course, pre] of prerequisites) {
+        preMap[course].push(pre);
+    }
+    var visited = new Set();
+    var dfs = (course) => {
+        if (visited.has(course)) {
+            return false;
+        }
+        if (preMap[course].length === 0) {
+            return true;
+        }
+        
+        visited.add(course);
+        for (pre of preMap[course]) {
+            if (!dfs(pre)) {
+                return false;
+            }
+        }
+        visited.delete(course);
+        preMap[course] = [];
+        return true;
+    }
+    
+    for (var course = 0; course < numCourses; course++) {
+        if (!dfs(course)) {
+            return false
         }
     }
-    var count = numCourses;
-    while (set.size < numCourses && count > 0) {
-        for (var key of Object.keys(preReqs)) {
-            // console.log(set.values());
-            var canBeTaken = true;
-            var needToBeTaken = preReqs[key];
-            for (var i = 0; i < needToBeTaken.length; i++) {
-                if (!set.has(needToBeTaken[i])) {
-                    canBeTaken = false;
-                }
-            }
-            if (canBeTaken) {
-                set.add(Number(key));
-            }
-        }
-        count--;
-    }
-    return set.size === numCourses;
+    return true;
 };
