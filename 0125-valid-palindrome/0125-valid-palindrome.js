b6f1603943d52012c5a6bde5dcd48582f4e6be8e
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2024-01-05 10:10:42 -0800

    Time: 59 ms (78.85%), Space: 48.1 MB (25.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -5,12 +5,14 @@
 var isPalindrome = function(s) {
     var str = "";
     for (var i = 0; i < s.length; i++) {
-        var cc = s[i].charCodeAt(0);
+        var char = s[i].toLowerCase();
+        var cc = char.charCodeAt(0);
 
-        if ((cc > 47 && cc < 58) || (cc > 64 && cc < 91) || (cc > 96 && cc < 123)) {
-            str += s[i].toLowerCase();
+        if ((cc > 47 && cc < 58) || (cc > 96 && cc < 123)) {
+            str += char;
         }
     }
+    // console.log(str);
     for (var i = 0; i < Math.floor(str.length/2); i++) {
         // console.log(str[i], str[str.length - 1 - i]);
         if (str[i] !== str[str.length - 1 - i]) {
