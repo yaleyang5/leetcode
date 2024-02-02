17f5d76d4650f10b67d201b829254cff3d5e300b
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2024-02-01 21:24:42 -0800

    Time: 41 ms (99.60%), Space: 51.4 MB (6.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,10 +6,12 @@
 var twoSum = function(nums, target) {
     var diff = {};
     for (var i = 0; i < nums.length; i++) {
-        if (diff[target - nums[i]] === undefined) {
-            diff[nums[i]] = i;
+        var num = nums[i];
+        var check = diff[num];
+        if (check === undefined) {
+            diff[target - num] = i;
         } else {
-            return [i, diff[target - nums[i]]];
+            return [i, check];
         }
     }
     return [-1, -1];
