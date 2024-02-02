6536d22bd69d0deac4e39af06c12cf17dd19a005
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2024-02-01 21:17:32 -0800

    Time: 65 ms (19.93%), Space: 52.2 MB (8.51%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -8,12 +8,13 @@ var search = function(nums, target) {
     var end = nums.length - 1;
     while (start <= end) {
         var mid = start + Math.floor((end - start) / 2);
-        if (target > nums[mid]) {
-            start = mid + 1;
-        } else if (target < nums[mid]) {
-            end = mid - 1;
-        } else {
+        if (target === nums[mid]) {
             return mid;
+        }
+        else if (target > nums[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
         }
     }
     return -1;
