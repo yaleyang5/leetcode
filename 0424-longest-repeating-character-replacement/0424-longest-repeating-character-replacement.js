622025869a2552a8df158ee1d99b2451f3903905
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2024-02-02 09:47:19 -0800

    Time: 71 ms (66.73%), Space: 50.1 MB (21.33%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -7,41 +7,16 @@ var characterReplacement = function(s, k) {
     var maxWindow = -1;
     
     var letters = {};
-    var keys = [];
-    var end = 0;
-    for (var start = 0; start < s.length; start++) {
-        if (keys.length === 0) {
-            for (var i = start; i <= end; i++) {
-                if (letters[s[i]] === undefined) {
-                    letters[s[i]] = 0;
-                    keys.push(s[i]);
-                }
-                letters[s[i]]++;
-            }
-        } else {
-            letters[s[start - 1]]--;
+    var start = 0;
+    var max = -1;
+    for (var end = 0; end < s.length; end++) {
+        letters[s[end]] = letters[s[end]] === undefined ? 1 : letters[s[end]] + 1;
+        max = Math.max(max, letters[s[end]]);
+        while (max + k < end - start + 1) {
+            letters[s[start]]--;
+            start++;
         }
-        // console.log(letters);
-        var max = -1;
-        for (var key of keys) {
-            max = Math.max(max, letters[key]);
-        }
-        while (max + k >= end - start + 1 && end < s.length) {
-            if (k === 0 && max !== end - start + 1) {
-                break;
-            }
-            maxWindow = Math.max(maxWindow, end - start + 1);
-            end++;
-            if (letters[s[end]] === undefined) {
-                letters[s[end]] = 0;
-                keys.push(s[end]);
-            }
-            letters[s[end]]++;
-            for (var key of keys) {
-                max = Math.max(max, letters[key]);
-            }
-        }
-        letters[start]--;
+        maxWindow = Math.max(maxWindow, end - start + 1);
     }
     return maxWindow;
 };
