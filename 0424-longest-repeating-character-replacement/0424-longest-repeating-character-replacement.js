8e801d4736dc32745bdc14fe454f80fb6a8d0453
Yale Yang <67299317+yaleyang5@users.noreply.github.com>
2024-02-02 09:13:56 -0800

    Time: 127 ms (35.20%), Space: 58.3 MB (5.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -4,21 +4,44 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    var count = {};
-    var maxLength = 0;
+    var maxWindow = -1;
     
-    var start = 0;
-    var max = 0;
-    for (var i = 0; i < s.length; i++) {
-        count[s[i]] = count[s[i]] ? count[s[i]] + 1 : 1;
-        max = Math.max(max, count[s[i]]);
-        
-        while (i - start + 1 - max > k) {
-            count[s[start]]--;
-            start++;
+    var letters = {};
+    var keys = [];
+    var end = 0;
+    for (var start = 0; start < s.length; start++) {
+        if (keys.length === 0) {
+            for (var i = start; i <= end; i++) {
+                if (letters[s[i]] === undefined) {
+                    letters[s[i]] = 0;
+                    keys.push(s[i]);
+                }
+                letters[s[i]]++;
+            }
+        } else {
+            letters[s[start - 1]]--;
         }
-
-        maxLength = Math.max(maxLength, i - start + 1);
+        // console.log(letters);
+        var max = -1;
+        for (var key of keys) {
+            max = Math.max(max, letters[key]);
+        }
+        while (max + k >= end - start + 1 && end < s.length) {
+            if (k === 0 && max !== end - start + 1) {
+                break;
+            }
+            maxWindow = Math.max(maxWindow, end - start + 1);
+            end++;
+            if (letters[s[end]] === undefined) {
+                letters[s[end]] = 0;
+                keys.push(s[end]);
+            }
+            letters[s[end]]++;
+            for (var key of keys) {
+                max = Math.max(max, letters[key]);
+            }
+        }
+        letters[start]--;
     }
-    return maxLength;
+    return maxWindow;
 };
